d34d100f55e4bfda002ef791d74cd03f32430805
Henry Brown <henry@ibespwn.com>
2018-09-27 21:17:41 -0700

    More data, more formatting

--- src/print_redstone_io.lua
+++ src/print_redstone_io.lua
@@ -3,4 +3,12 @@ local sides = require("sides")
 
 local rs = component.redstone
 
+print("N\tE\tS\tW\n")
 print(rs.getInput(sides.north))
+print("\t")
+print(rs.getInput(sides.east))
+print("\t")
+print(rs.getInput(sides.south))
+print("\t")
+print(rs.getInput(sides.west))
+print("\n")
